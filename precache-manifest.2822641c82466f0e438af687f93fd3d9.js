3d58eecfb85b617c3782afb7c52c40ac0a0426c0
David Akpan <daviduwana@gmail.com>
2020-04-26 23:01:11 +0100

    Updates

---
+++ precache-manifest.2822641c82466f0e438af687f93fd3d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "888b5a863de00c6c8b57086bef305423",
+    "url": "/react-hooks/index.html"
+  },
+  {
+    "revision": "5c507e6148e95be6525c",
+    "url": "/react-hooks/static/css/main.41b1173f.chunk.css"
+  },
+  {
+    "revision": "49efaae98686a5897881",
+    "url": "/react-hooks/static/js/2.b7cbaac4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-hooks/static/js/2.b7cbaac4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c507e6148e95be6525c",
+    "url": "/react-hooks/static/js/main.51c0b7ca.chunk.js"
+  },
+  {
+    "revision": "9bbfe1905e01c3fcc8a3",
+    "url": "/react-hooks/static/js/runtime-main.1e55bb6c.js"
+  }
+]);
